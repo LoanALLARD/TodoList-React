59406002603429a2c3da103ad893e8777e32b702
Loan <loan.allard@etu.univ-amu.fr>
2026-03-27 15:59:26 +0100

    feat: enhance task display with additional details in TaskList component

--- todo-list/src/TasksList.js
+++ todo-list/src/TasksList.js
@@ -8,7 +8,11 @@ export default function TaskList({ tasks }) {
                 <ul>
                     {tasks.map((task) => (
                         <li key={task.id}>
-                            Identifiant: {task.id} - Tâche: {task.title}
+                            Identifiant: {task.id} - Tâche: {task.title}-
+                            Statut: {task.status} - Date d'échéance:
+                            {task.date_echeance} - Date de création:{" "}
+                            {task.date_creation} - Status: {task.etat} - Assigné
+                            à: {task.equipiers ? task.equipiers.map(e => e.name).join(", ") : "Personne"}
                         </li>
                     ))}
                 </ul>
